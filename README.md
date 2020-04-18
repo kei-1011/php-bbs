2626499b55a88ce5c13548f67dacb6af208273e1
kohei <k_kojima0407@yahoo.co.jp>
2020-04-18 13:47:55 +0900

    SELECT

--- README.md
+++ README.md
@@ -2,3 +2,57 @@
 一言掲示板の作成
 
 PHPの基礎学習のためのアウトプット
+
+
+## テキストファイルからデータを取得する場合
+
+```php
+    if ($file_handle = fopen(FILENAME, "a")) {
+      /*
+      fopenでファイルを開く
+      1：ファイル名を含めたパス
+      2：モード　読み込みだけを行う「r」、書き込みを行う「w」や「a」などが
+        「w」はファイル内容を一旦リセットして書き込みを行い、「a」は末端から追記する形で書き込みを行う
+      */
+
+      //書き込み日時を取得
+      //サーバーでで世界標準時間になっている場合があるため
+      $now_date = date("Y-m-d H:i:s");
+
+      //書き込むデータを作成
+      //「‘ (シングルクォーテーション)」で囲み、「表示名」「メッセージ」「投稿日時」をそれぞれ「, (コンマ)」で区切る
+      $data = "'" . $clean['view_name'] . "','" . $clean['message'] . "','" . $now_date . "'\n";
+
+      //書き込み
+      fwrite($file_handle, $data);
+
+      //ファイルを安全に閉じる関数（fopenとセットで使う）
+      fclose($file_handle);
+
+      $success_message = "メッセージを書き込みました";
+    }
+```
+
+## テキストファイルから書き込む
+
+```php
+
+//テキストファイルからの読み込み
+if ($file_handle = fopen(FILENAME, 'r')) {
+  while ($data = fgets($file_handle)) {   //ファイルから1行ずつデータを取り出す fgets
+
+    $split_data = preg_split('/\'/', $data);
+    //preg_split関数は文字列を特定の文字で分割する関数「'」で分割
+
+    // 一旦messageに入れる
+    $message = array(
+      'view_name' => $split_data[1],
+      'message' => $split_data[3],
+      'post_date' => $split_data[5]
+    );
+    array_unshift($message_array, $message);    //$message_arrayに入れる
+  }
+  //ファイルを閉じる
+  fclose($file_handle);
+}
+```
--- index.php
+++ index.php
@@ -56,36 +56,11 @@ if (!empty($_POST['btn_submit'])) {
     $clean['message'] = htmlspecialchars($_POST['message'], ENT_QUOTES);
     $clean['message'] = preg_replace('/\\r\\n|\\n|\\r/', '<br>', $clean['message']);  // 改行コードがあればbrに置き換え
   }
-//エラーがなければ、$cleanにHTMLエンティティ化して格納
+  //エラーがなければ、$cleanにHTMLエンティティ化して格納
 
 
   //エラーがなければ書き込みを実行
   if( empty($error_message) ){
-
-    /*
-    if ($file_handle = fopen(FILENAME, "a")) {
-      fopenでファイルを開く
-      1：ファイル名を含めたパス
-      2：モード　読み込みだけを行う「r」、書き込みを行う「w」や「a」などが
-        「w」はファイル内容を一旦リセットして書き込みを行い、「a」は末端から追記する形で書き込みを行う
-
-      //書き込み日時を取得
-      //サーバーでで世界標準時間になっている場合があるため
-      $now_date = date("Y-m-d H:i:s");
-
-      //書き込むデータを作成
-      //「‘ (シングルクォーテーション)」で囲み、「表示名」「メッセージ」「投稿日時」をそれぞれ「, (コンマ)」で区切る
-      $data = "'" . $clean['view_name'] . "','" . $clean['message'] . "','" . $now_date . "'\n";
-
-      //書き込み
-      fwrite($file_handle, $data);
-
-      //ファイルを安全に閉じる関数（fopenとセットで使う）
-      fclose($file_handle);
-
-      $success_message = "メッセージを書き込みました";
-    }
-  */
 
     // データベースに接続
     $mysqli = new mysqli('localhost', 'root', 'root', 'php_bbs');
@@ -119,23 +94,33 @@ if (!empty($_POST['btn_submit'])) {
   }
 }
 
-//テキストファイルからの読み込み
-if ($file_handle = fopen(FILENAME, 'r')) {
-  while ($data = fgets($file_handle)) {   //ファイルから1行ずつデータを取り出す fgets
 
-    $split_data = preg_split('/\'/', $data);
-    //preg_split関数は文字列を特定の文字で分割する関数「'」で分割
+// データベースに接続
+$mysqli = new mysqli('localhost', 'root', 'root', 'php_bbs');
 
-    // 一旦messageに入れる
-    $message = array(
-      'view_name' => $split_data[1],
-      'message' => $split_data[3],
-      'post_date' => $split_data[5]
-    );
-    array_unshift($message_array, $message);    //$message_arrayに入れる
+// 接続エラーの確認
+if ($mysqli->connect_errno) {
+  $error_message[] = '書き込みに失敗しました。 エラー番号 ' . $mysqli->connect_errno . ' : ' . $mysqli->connect_error;
+} else {
+  // 書き込み処理
+
+  $sql = "SELECT view_name,message,post_date FROM message ORDER BY post_date DESC";
+  /* - 基本的な文法 -
+  SELECT 取得するカラム名 FROM テーブル名 WHERE 取得条件 ORDER BY ソートするカラム名 DESC */
+
+  $res = $mysqli->query($sql);  // queryメソッドで実行
+
+  if($res) {
+    $message_array = $res->fetch_all(MYSQLI_ASSOC);
+    /*
+    fetch_allで全てのデータを取得
+    MYSQLI_ASSOCでファイル読み込みの時と同様の配列型式で取得
+
+    一覧に出力するため、$message_arrayに$resを格納する
+    */
   }
-  //ファイルを閉じる
-  fclose($file_handle);
+
+  $mysqli->close();
 }
 ?>
   <h1>ひと言掲示板</h1>
